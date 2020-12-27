a166bb72786d33ae5117368254f66e069447f22e
JckD <c16335383@mydit.ie>
2020-12-27 01:43:49 +0000

    fixed it?

--- src/components/test.component.js
+++ src/components/test.component.js
@@ -5,6 +5,9 @@ import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 //import { STLLoader } from 'https://cdn.jsdelivr.net/npm/three@0.120.1/examples/jsm/loaders/STLLoader.js';
 import foxy from '../3Dfiles/foxy.stl';
+import dragon from '../../3Dfiles/red-dragon-adult-updated.stl';
+import wall from '../../3Dfiles/wall.stl';
+
 
 export default class CreateQuote extends Component {
 
@@ -12,199 +15,184 @@ export default class CreateQuote extends Component {
 
 
     componentDidMount(){
-    //     var scene = new THREE.Scene();
-    //     var loader = new STLLoader();
-
-      
-    //     scene.background = new THREE.Color( 0xc4c4c4 );
-    //    // === THREE.JS CODE START ===
-        
-    //     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    //     var renderer = new THREE.WebGLRenderer({ alpha: true });
-    //     renderer.setSize( window.innerWidth, window.innerHeight );
-
-    //     const controls = new OrbitControls(camera, renderer.domElement)
-    //     controls.enableDamping = true
-    //     // //document.body.appendChild( renderer.domElement );
-    //     this.mount.appendChild( renderer.domElement );
-    //      var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    //      var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    //      var cube = new THREE.Mesh( geometry, material );
-    //      scene.add( cube );
-        
-    //     loader.load('../3Dfiles/foxy.stl', function(geometry) {
-    //         var material = new THREE.MeshBasicMaterial( { color: 0xFFFFFF } );
-    //         var mesh = new THREE.Mesh(geometry)
-    //        mesh.position.set( 0, - 0.25, 0.6 );
-	// 		//mesh.rotation.set( 0, - Math.PI / 2, 0 );
-	// 		//mesh.scale.set( 0.5, 0.5, 0.5 );
-    //         //mesh.castShadow = true;
-    //         //mesh.receiveShadow = true;
-    //         mesh.position.set( 0, 20, 0);
-    //         scene.add(mesh)
-    //     },  // called when loading is in progresses
-    //     ( xhr ) => {
-
-    //         console.log((xhr.loaded / xhr.total * 100) + '% loaded')
-
-    //    }, (error) => {
-    //         console.log(error)
-    //     })
-    //     camera.position.z = 5;
-    //     renderer.render( scene, camera );
-        // var animate = function () {
-        //     requestAnimationFrame( animate );
-        //     cube.rotation.x += 0.01;
-        //     cube.rotation.y += 0.01;
-        //     renderer.render( scene, camera );
-        // };
-        //animate();
-        // === THREE.JS EXAMPLE CODE END ===
 
         // Necessary for camera/plane rotation
-			var degree = Math.PI/180;
+        var degree = Math.PI/180;
 
-			// Setup
-			var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-			var renderer = new THREE.WebGLRenderer();
+        // Setup
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        var renderer = new THREE.WebGLRenderer();
 
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            renderer.shadowMap.enabled = true;
-          
-            //document.body.appendChild(renderer.domElement);
-            this.mount.appendChild( renderer.domElement );
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.shadowMap.enabled = true;
+        
+        //document.body.appendChild(renderer.domElement);
+        this.mount.appendChild( renderer.domElement );
 
-			// Resize after viewport-size-change
-			window.addEventListener("resize", function () {
-			    var height = window.innerHeight;
-			    var width = window.innerWidth;
-			    renderer.setSize(width, height);
-			    camera.aspect = width / height;
-			    camera.updateProjectionMatrix();
-			});
+        // Resize after viewport-size-change
+        window.addEventListener("resize", function () {
+            var height = window.innerHeight;
+            var width = window.innerWidth;
+            renderer.setSize(width, height);
+            camera.aspect = width / height;
+            camera.updateProjectionMatrix();
+        });
 
-			// Adding controls
-			var controls = new OrbitControls(camera, renderer.domElement);
+        // Adding controls
+        var controls = new OrbitControls(camera, renderer.domElement);
 
-			// Ground (comment out line: "scene.add( plane );" if Ground is not needed...)
-			var plane = new THREE.Mesh(
-			    new THREE.PlaneBufferGeometry(500, 500 ),
-			    new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } )
-			);
-			plane.rotation.x = -90 * degree;
-            plane.position.y = 0;
-            plane.receiveShadow = true;
-            plane.castShadow = true;
-            plane.name = "plane"
-			scene.add( plane );
+        // Ground (comment out line: "scene.add( plane );" if Ground is not needed...)
+        var plane = new THREE.Mesh(
+            new THREE.PlaneBufferGeometry(500, 500 ),
+            new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } )
+        );
+        plane.rotation.x = -90 * degree;
+        plane.position.y = 0;
+        plane.receiveShadow = true;
+        plane.castShadow = true;
+        plane.name = "plane"
+        scene.add( plane );
+        
+        
+
+        var loader = new STLLoader();
+        // Binary files - STL Import
+        loader.load( foxy, function ( geometry ) {
+            var material = new THREE.MeshPhongMaterial( { color: 0xc60b0b} );
             
-          
-
-			var loader = new STLLoader();
-			// Binary files - STL Import
-            loader.load( foxy, function ( geometry ) {
-                var material = new THREE.MeshPhongMaterial( { color: 0x2194ce} );
-                
-			    var mesh = new THREE.Mesh( geometry, material );
-                mesh.position.set( 0, 1, 0);
-                mesh.rotation.x  = -90 * degree;
-                mesh.rotation.z = 90 * degree
-                mesh.receiveShadow = true;
-                mesh.castShadow = true;
-                
-                mesh.name = 'foxy'
-                //mesh.traverse(function(child){child.castShadow = true;});
-			    scene.add( mesh );
-            } );
+            var mesh = new THREE.Mesh( geometry, material );
+            mesh.position.set( 0, 1, 0);
+            mesh.rotation.x  = -90 * degree;
+            mesh.rotation.z = 90 * degree
+        
+            mesh.receiveShadow = true;
+            mesh.castShadow = true;
             
-            var geometry = new THREE.BoxGeometry( 50, 50, 50 );
+            mesh.name = 'foxy'
+            //mesh.traverse(function(child){child.castShadow = true;});
+            scene.add( mesh );
+        } );
 
-            var material = new THREE.MeshPhongMaterial( { color: 0x2194ce } );
-            var cube = new THREE.Mesh( geometry, material );
-            cube.position.set(-50, 25, -50)
-            cube.castShadow = true;
-            cube.receiveShadow = true;
-            scene.add( cube );
-
-			// Camera positioning
-			camera.position.z = 100;
-			camera.position.y = 100;
-			camera.rotation.x = -45 * degree;
-
-			// Ambient light (necessary for Phong/Lambert-materials, not for Basic)
-            var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-            scene.add(ambientLight);
+        loader.load( dragon, function ( geometry ) {
+            var material = new THREE.MeshPhongMaterial( { color: 0xc60b0b } );
             
-            //const light = new THREE.HemisphereLight( 0xffffff, 0x080820, 1 );
-            //scene.add( light );
-            var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-            directionalLight.position.set(0, 300, 0)
-            directionalLight.rotation.x = -45 * degree
-            directionalLight.target.position.set(0,0,0)
-            directionalLight.castShadow = true;
+            var mesh = new THREE.Mesh( geometry, material );
+            mesh.position.set( -50, 1, 50);
+            mesh.rotation.x  = -90 * degree;
+            mesh.rotation.z = 90 * degree
+        
+            mesh.receiveShadow = true;
+            mesh.castShadow = true;
             
-            directionalLight.target.name = "target"
-            //scene.add( directionalLight );
-            scene.add(directionalLight.target)
+            mesh.name = 'dragon'
+            //mesh.traverse(function(child){child.castShadow = true;});
+            scene.add( mesh );
+        } );
 
-
-            directionalLight.shadow.mapSize.width = 512; // default
-            directionalLight.shadow.mapSize.height = 512; // default
-            directionalLight.shadow.camera.near = 0.1; // default
-            directionalLight.shadow.camera.far = 500; // default
+        loader.load( wall, function ( geometry ) {
+            var material = new THREE.MeshPhongMaterial( { color: 0x757575 } );
             
-
-            const spotLight = new THREE.SpotLight( 0xffffff );
-            spotLight.position.set(200, 200, 0 );
-            spotLight.angle = Math.PI / 4;
-            spotLight.castShadow = true;
-            spotLight.penumbra = 0.1;
-            spotLight.shadow.mapSize.width = 1024;
-            spotLight.shadow.mapSize.height = 1024;
-
-            spotLight.shadow.camera.near = 5;
-            spotLight.shadow.camera.far = 4000;
-            spotLight.shadow.camera.fov = 1;
-            spotLight.shadow.focus = .8;
-            scene.add( spotLight );
-
-            const helper = new THREE.DirectionalLightHelper( directionalLight, 5 );
-            //scene.add( helper );
-            const helper2 = new THREE.SpotLightHelper( spotLight, 5 );
-            scene.add( helper2 );
-
-            const shadowCameraHelper = new THREE.CameraHelper( spotLight.shadow.camera );
-			scene.add( shadowCameraHelper );
-           
-
-
-            console.log(scene)
-
-
-            scene.traverse( function( child ) { 
-
-                if ( child.isMesh ) {
+            var mesh = new THREE.Mesh( geometry, material );
+            mesh.position.set( 50, -6, 50);
+            mesh.rotation.x  = -90 * degree;
+            mesh.rotation.z = 90 * degree
+        
+            mesh.receiveShadow = true;
+            mesh.castShadow = true;
             
-                    child.castShadow = true;
-                    child.receiveShadow = true;
-            
-                }
-            
-            } );
-			// Draw scene
-			var render = function () {
-			    renderer.render(scene, camera);
-			};
+            mesh.name = 'dragon'
+            //mesh.traverse(function(child){child.castShadow = true;});
+            scene.add( mesh );
+        } );
+        
+        // var geometry = new THREE.BoxGeometry( 50, 50, 50 );
 
-			// Run game loop (render,repeat)
-			var GameLoop = function () {
-			    requestAnimationFrame(GameLoop);
+        // var material = new THREE.MeshPhongMaterial( { color: 0x2194ce } );
+        // var cube = new THREE.Mesh( geometry, material );
+        // cube.position.set(-50, 25, -50)
+        // cube.castShadow = true;
+        // cube.receiveShadow = false;
+        // scene.add( cube );
 
-			    render();
-			};
+        // Camera positioning
+        camera.position.z = 100;
+        camera.position.y = 100;
+        camera.rotation.x = -45 * degree;
 
-			GameLoop();
+        // Ambient light (necessary for Phong/Lambert-materials, not for Basic)
+        var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+        scene.add(ambientLight);
+        
+        //const light = new THREE.HemisphereLight( 0xffffff, 0x080820, 1 );
+        //scene.add( light );
+        var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+        directionalLight.position.set(0, 300, 0)
+        directionalLight.rotation.x = -45 * degree
+        directionalLight.target.position.set(0,0,0)
+        directionalLight.castShadow = true;
+        
+        directionalLight.target.name = "target"
+        //scene.add( directionalLight );
+        scene.add(directionalLight.target)
+
+
+        directionalLight.shadow.mapSize.width = 512; // default
+        directionalLight.shadow.mapSize.height = 512; // default
+        directionalLight.shadow.camera.near = 0.1; // default
+        directionalLight.shadow.camera.far = 500; // default
+        
+
+        const spotLight = new THREE.SpotLight( 0xffffff );
+        spotLight.position.set(200, 200, 0 );
+        spotLight.angle = Math.PI / 4;
+        spotLight.castShadow = true;
+        spotLight.penumbra = 0.1;
+        spotLight.shadow.mapSize.width = 1024;
+        spotLight.shadow.mapSize.height = 1024;
+
+        spotLight.shadow.camera.near = 5;
+        spotLight.shadow.camera.far = 4000;
+        spotLight.shadow.camera.fov = 1;
+        spotLight.shadow.focus = .8;
+        scene.add( spotLight );
+
+        const helper = new THREE.DirectionalLightHelper( directionalLight, 5 );
+        //scene.add( helper );
+        const helper2 = new THREE.SpotLightHelper( spotLight, 5 );
+        scene.add( helper2 );
+
+        const shadowCameraHelper = new THREE.CameraHelper( spotLight.shadow.camera );
+        scene.add( shadowCameraHelper );
+        
+
+
+        console.log(scene.f)
+
+
+        scene.traverse( function( child ) { 
+
+            if ( child.isMesh ) {
+        
+                child.castShadow = true;
+                child.receiveShadow = true;
+        
+            }
+        
+        } );
+        // Draw scene
+        var render = function () {
+            renderer.render(scene, camera);
+        };
+
+        // Run game loop (render,repeat)
+        var GameLoop = function () {
+            requestAnimationFrame(GameLoop);
+
+            render();
+        };
+
+        GameLoop();
 
 
     }
